66e1e910fea8fdb409514ee61f56ef369d849ed7
Ryan Melnyk <schmelnyk@gmail.com>
2024-03-13 16:50:13 -0700

    functional map_ont_reads.py
    - added bioinformatics steps

--- README.md
+++ README.md
@@ -6,15 +6,16 @@ Scripts for aligning ONT reads to plasmids
 
 I haven't created a yaml file yet as I'm not sure if this is a pipeline sort of thing that merits nextflow integration or just a collection of scripts. For now I'm just listing the required python and command line tools below.
 
+I installed minimap2/samtools using `homebrew` in OSX and managed python packages with the pip installed by conda in my base environment.
 
 ```
-conda install bioconda::minimap2
-conda install bioconda::samtools
+minimap2 v2.27-r1193
+samtools v1.19.2
 
 pip install Bio
 ```
 
 ### Input data
 
-Reference fasta file of expected plasmid (e.g. `test/reference.fa`)
-FASTQ reads from nanopore (e.g. `test/reads.fastq`)
+- Reference fasta file of expected plasmid (e.g. `test/reference.fa`)
+- FASTQ reads from nanopore (e.g. `test/reads.fastq`)
--- map_ont_reads.py
+++ map_ont_reads.py
@@ -3,9 +3,13 @@
 
 import argparse
 import os
-import sys
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import re
 import subprocess
 from Bio import SeqIO
+from Bio.Seq import Seq
 
 
 def parse_args():
@@ -28,29 +32,81 @@ def parse_args():
     return parser.parse_args()
 
 
-def prep_ref(ref):
-    # read in plasmid sequence
-    seqs = []
-    for seq in SeqIO.parse(open(ref), 'fasta'):
-        seqs.append(seq)
-    if len(seqs) != 1:
-        print("Check number of sequences in FASTA file...exiting.")
-        sys.exit()
-    
-    # duplicate and concatenate plasmid sequence
-    seqs[0].seq = seqs[0].seq*2
-    o = open("tmp/concat_ref.fa", 'w')
-    SeqIO.write(seq, o, 'fasta')
-    o.close()
-    return
-
-
-def minimap(reads):
+def minimap_and_samtools(reads, ref):
     cmd = f"""
-    minimap2 -x map-ont -a -o tmp/{prefix}.sam tmp/concat_ref.fa {reads}
+    minimap2 \
+        -a -x map-ont \
+        --secondary=no \
+        -o tmp/{prefix}.sam \
+        {ref} \
+        {reads}
     """
     proc = subprocess.Popen(cmd.split())
     proc.wait()
+
+    cmd = f"""
+    samtools view -Sb tmp/{prefix}.sam -o tmp/{prefix}.bam
+    """
+    proc = subprocess.Popen(cmd.split())
+    proc.wait()
+
+    cmd = f"""
+    samtools sort tmp/{prefix}.bam -o tmp/{prefix}.sorted.bam
+    """
+    proc = subprocess.Popen(cmd.split())
+    proc.wait()
+
+    cmd = f"""
+    samtools mpileup \
+        --reference {ref} \
+        -o tmp/{prefix}.mpile.txt \
+        tmp/{prefix}.sorted.bam
+    """
+    proc = subprocess.Popen(cmd.split())
+    proc.wait()
+    return
+
+
+def read_data(reads_path):
+    # get length of sequences
+    seq_data = {}
+    for seq in SeqIO.parse(open(reads_path), 'fastq'):
+        seq_data[seq.id] = [len(seq), False, None]
+
+    # was the read mappable to the reference?
+    bc_regex = "CGAGGTCTCT([ATGCN]{20})CGTACGCTGC"
+    for line in open("tmp/output.sam"):
+        if line.startswith("@"):
+            continue
+        else:
+            vals = line.rstrip().split()
+            if vals[2] != "*":
+                seq_data[vals[0]][1] = True
+                if int(vals[1]) in [16, 2064]:
+                    query = str(Seq(vals[9]).reverse_complement())
+                else:
+                    query = vals[9]
+                res = re.search(bc_regex, query)
+                if res == None:
+                    pass
+                else:
+                    seq_data[vals[0]][2] = res.group(1)
+    df = pd.DataFrame.from_dict(seq_data, orient="index")
+    df.columns = ["length", "mapped", "barcode"]
+    df.to_csv(f"tmp/{prefix}.csv")
+    return df
+
+
+def plot(seq_df):
+    sns.histplot(
+        data=seq_df,
+        x="length",
+        hue="mapped",
+        multiple="stack",
+        binwidth=100)
+    plt.xlim(0,10000)
+    plt.savefig(f"tmp/{prefix}.hist.png", dpi=300)
+    return
 
 
 def main():
@@ -63,10 +119,10 @@ def main():
     else:
         prefix = "output"
 
-    prep_ref(ref_path)
-    minimap(reads_path)
-    # make histogram of read lengths
-    # % of reads that align?
+    minimap_and_samtools(reads_path, ref_path)
+
+    seq_df = read_data(reads_path)
+    plot(seq_df)
 
 
 if __name__ == '__main__':
