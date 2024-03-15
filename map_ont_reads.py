cafde8298c9a7d327bcad32aa7b95b5f54fbfe70
Ryan Melnyk <schmelnyk@gmail.com>
2024-03-14 17:16:37 -0700

    fix linting and harcoded prefix

--- map_ont_reads.py
+++ map_ont_reads.py
@@ -73,9 +73,9 @@ def read_data(reads_path):
     for seq in SeqIO.parse(open(reads_path), 'fastq'):
         seq_data[seq.id] = [len(seq), False, None]
 
-    # was the read mappable to the reference?
+    # check if read mapped to reference and try to extract barcode
     bc_regex = "CGAGGTCTCT([ATGCN]{20})CGTACGCTGC"
-    for line in open("tmp/output.sam"):
+    for line in open(f"tmp/{prefix}.sam"):
         if line.startswith("@"):
             continue
         else:
@@ -87,7 +87,7 @@ def read_data(reads_path):
                 else:
                     query = vals[9]
                 res = re.search(bc_regex, query)
-                if res == None:
+                if res is None:
                     pass
                 else:
                     seq_data[vals[0]][2] = res.group(1)
@@ -104,7 +104,7 @@ def plot(seq_df):
         hue="mapped",
         multiple="stack",
         binwidth=100)
-    plt.xlim(0,10000)
+    plt.xlim(0, 10000)
     plt.savefig(f"tmp/{prefix}.hist.png", dpi=300)
     return
 
